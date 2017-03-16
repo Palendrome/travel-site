d26505f993c726de3dcc280659a63d1c1366e1f4
Paul <spstudio@iafrica.com>
2017-03-16 14:44:43 +0200

    error handling for watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,5 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssimport = require('postcss-import'),
-browserSync = require('browser-sync').create();
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default',function(){
-	console.log("Hurray, you mad a gulp Task");
-});
 
-gulp.task('html', function() {
-	console.log("imagine something useful here");
-});
-
-gulp.task('styles', function() {
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-	
-	
-});
-
-gulp.task('watch', function(){
-	
-	browserSync.init({
-		notify:false,
-		server:{
-			baseDir: "app"
-		}
-	});
-	
-	watch('./app/index.html', function(){
-		browserSync.reload();
-	});
-	
-	watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('cssInject');
-	});
-});
-
-gulp.task('cssInject', ['styles'], function() {
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
-});
 
